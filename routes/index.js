15e45bca68d8f70d9fc10caa59dc80e4b425cdca
xinyue <xinyue@home.com>
2018-01-18 23:08:43 +0800

    初步搭建项目

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var router = require('koa-router')();
+
+router.get('/', function *(next) {
+  yield this.render('index', {
+    title: 'Hello World Koa!'
+  });
+});
+
+router.get('/foo', function *(next) {
+  yield this.render('index', {
+    title: 'Hello World foo!'
+  });
+});
+
+router.get('/allCake', function *(next) {
+    yield this.render('index', {
+        title: 'Hello all cake!'
+    });
+});
+
+router.get('/allCake2', function *(next) {
+    yield this.render('index', {
+        title: 'Hello all cake 2!'
+    });
+});
+
+router.get('/test', function *(next) {
+    yield this.render('test', {
+        title: 'Hello test!'
+    });
+});
+module.exports = router;
