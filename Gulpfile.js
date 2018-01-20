bdbaab8afd940f8b1c1adc4e37d43e17de198b71
xinyue <xinyue@home.com>
2018-01-21 01:17:04 +0800

    gulp

---
+++ Gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var bower = require('gulp-bower');
+
+// 自动分配手写类库引入
+gulp.task('bower', function() {
+    gulp.src('./node_modules/jquery/dist/jquery.min.js')
+        .pipe(gulp.dest('./public/js/lib/'));
+});
